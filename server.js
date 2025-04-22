b205432f230bb13cc9e9f7645402f20423ef79be
Basudev Nayak <51286951+basudevnayak@users.noreply.github.com>
2025-04-22 17:07:34 +0530

    addd

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Mongoose schema and model
 const productSchema = new mongoose.Schema({
